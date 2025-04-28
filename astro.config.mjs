efda33c7ab9fb40e55a8ad18b230edbc17f2c813
Michael Campbell <mt.campbell@outlook.com>
2025-04-28 16:40:34 +0100

    adds rss feed

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
-  adapter: vercel()
+    site: "https://www.codingnomad.co.uk/",
+    output: "server",
+    adapter: vercel()
 });
