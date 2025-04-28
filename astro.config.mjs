cecb83756f8957df14cdba3b3a60e08ee68800ae
Michael Campbell <mt.campbell@outlook.com>
2025-04-28 16:45:32 +0100

    adds preact island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://www.codingnomad.co.uk/",
-    output: "server",
-    adapter: vercel()
+  site: "https://www.codingnomad.co.uk/",
+  output: "server",
+  adapter: vercel(),
+  integrations: [preact()]
 });
